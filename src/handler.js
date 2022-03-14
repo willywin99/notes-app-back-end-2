6737113d6419c32080e5c2dc709d0c87ea37e832
willywin99 <willywinsianturi@gmail.com>
2022-03-14 17:42:46 +0700

    fix typo in editNoteByIdHandler message response

--- src/handler.js
+++ src/handler.js
@@ -84,7 +84,7 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diperbaharui',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
